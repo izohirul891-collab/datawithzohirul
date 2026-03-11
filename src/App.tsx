76da4050ef95521e8a6cb7d17d0ac96d28289186
izohirul891-collab <izohirul891@gmail.com>
2026-03-11 20:02:34 +0600

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>MD. ZOHIRUL ISLAM</h1>
+      <p>Data Analytics Portfolio</p>
+    </div>
+  );
+}
